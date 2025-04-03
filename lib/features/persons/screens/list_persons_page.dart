1564862b12b1b3d2ccd386caeea84fcb987be887
EAhumada08 <emaahmc08@gmail.com>
2025-04-02 18:13:19 -0600

    crear pagina para listar personas y lista de paginas para el menu principal

---
+++ lib/features/persons/screens/list_persons_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListPersonsPage extends StatelessWidget {
+  const ListPersonsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Lista de Personas');
+  }
+}
---
+++ lib/routes/pages_list.dart
@@ -0,0 +1,18 @@
+import 'package:cooper_app/features/persons/screens/list_persons_page.dart';
+import 'package:flutter/widgets.dart';
+
+List<Widget> pagesList = [
+  ListPersonsPage(),
+  // Add your page widgets here
+  // Example:
+  // const Page1(),
+  // const Page2(),
+  // const Page3(),
+  // const Page4(),
+  // const Page5(),
+  // const Page6(),
+  // const Page7(),
+  // const Page8(),
+  // const Page9(),
+  // const Page10(),
+];
